6d3da65796837e2e075a34c67f2c1cc0b0249ce5
adexcel171 <godwillokenyi@gmail.com>
2024-01-30 18:47:27 +0100

    good changes

--- app/components/PostMain.tsx
+++ app/components/PostMain.tsx
@@ -44,16 +44,16 @@ export default function PostMain({ post }: PostMainCompTypes) {
                         </button>
                     </div>
                     <p className="text-[15px] pb-0.5 break-words md:max-w-[400px] max-w-[300px]">{post.text}</p>
-                    <p className="text-[14px] text-gray-500 pb-0.5">#fun #cool #SuperAwesome</p>
+                    <p className="text-[14px] text-gray-500 pb-0.5">#fun #tech #SuperAwesome</p>
                     <p className="text-[14px] pb-0.5 flex items-center font-semibold">
                         <ImMusic size="17"/>
-                        <span className="px-1">original sound - AWESOME</span>
+                        <span className="px-1"> AWESOME</span>
                         <AiFillHeart size="20"/>
                     </p>
 
                     <div className="mt-2.5 flex">
                         <div
-                            className="relative min-h-[480px] max-h-[580px] max-w-[260px] flex items-center bg-black rounded-xl cursor-pointer"
+                            className="relative min-h-[450px] max-h-[580px] max-w-[400px] flex items-center bg-black rounded-xl cursor-pointer"
                         >
                             <video 
                                 id={`video-${post.id}`}
@@ -66,7 +66,7 @@ export default function PostMain({ post }: PostMainCompTypes) {
                             <img 
                                 className="absolute right-2 bottom-10" 
                                 width="90" 
-                                src="/images/tiktok-logo-white.png"
+                                src="/images/admire.jpg"
                             />
                         </div>
                         
